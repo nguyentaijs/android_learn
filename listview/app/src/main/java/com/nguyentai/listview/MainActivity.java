d08adb52daf221aad424b8b2b574d0f435db6c8e
TaiNT <ca_admin@kt.com>
2020-12-19 11:51:03 +0700

    Click on item toast listview info

--- listview/app/src/main/java/com/nguyentai/listview/MainActivity.java
+++ listview/app/src/main/java/com/nguyentai/listview/MainActivity.java
@@ -3,8 +3,11 @@ package com.nguyentai.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,5 +31,20 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lstName = (ListView) findViewById(R.id.lstNames);
         lstName.setAdapter(adapterNames);
+
+        lstName.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, names.get(position), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        lstName.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, names.get(position), Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
     }
 }
