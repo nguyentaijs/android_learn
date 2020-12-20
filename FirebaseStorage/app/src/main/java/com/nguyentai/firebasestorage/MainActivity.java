d7071a3e18f97195c4e20d68a41964d46828d73d
TaiNT <ca_admin@kt.com>
2020-12-20 11:18:02 +0700

    UPload to Firebase storage using putBytes function

--- FirebaseStorage/app/src/main/java/com/nguyentai/firebasestorage/MainActivity.java
+++ FirebaseStorage/app/src/main/java/com/nguyentai/firebasestorage/MainActivity.java
@@ -1,22 +1,38 @@
 package com.nguyentai.firebasestorage;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 100;
+
+    private static final String TAG = "MainActivity";
+
     ImageView imgView;
     Button btnUpload;
+    FirebaseStorage fStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +45,41 @@ public class MainActivity extends AppCompatActivity {
     private void init() {
         imgView = (ImageView) findViewById(R.id.imageViewImage);
         btnUpload = (Button) findViewById(R.id.buttonUpload);
+        fStorage = FirebaseStorage.getInstance("gs://fir-db-be499.appspot.com");
 
         // On click even btnUpload
         btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Upload to Firebase storage
+                StorageReference sRef = fStorage.getReference();
+
+                StorageReference imgRef = sRef.child("captureImg" + System.currentTimeMillis());
+
+                imgView.setDrawingCacheEnabled(true);
+                imgView.buildDrawingCache();
+
+                Bitmap imgBitmap = ((BitmapDrawable) imgView.getDrawable()).getBitmap();
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                imgBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                byte[] imgbytes = baos.toByteArray();
+
+                UploadTask uploadTask = imgRef.putBytes(imgbytes);
+
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Error", e);
+                        Toast.makeText(MainActivity.this, "Failed, msg:" + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Log.i(TAG, taskSnapshot.getMetadata().toString());
+                        Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
